93a15b7fbe0038f1a220da8c6e3ec2fd8955f85b
Aaron Rohyans <arohyans@zscaler.com>
2023-06-12 22:06:21 -0700

    New ZL recipe

---
+++ Cloud and Branch Connector/terraform/labs/AWS-Zenith-Live/versions.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version  = "~> 4.2.0"
+      configuration_aliases = [aws.region1, aws.region2]
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    zpa = {
+      source  = "zscaler/zpa"
+      version = "2.7.7"
+    }
+  }
+  required_version = ">= 0.13"
+}
