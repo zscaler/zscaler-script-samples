d9a5ae33d8a3e9ecedc89e538166df8a5385e380
vikasksrivastava <vikas.k.srivastava@gmail.com>
2023-10-26 14:49:59 -0400

    Update setup1.ps1

--- ZDX/utils/setup1.ps1
+++ ZDX/utils/setup1.ps1
@@ -7,7 +7,7 @@ $file = "C:\Users\zdx\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Star
 # Basic Setup 
 
 Invoke-WebRequest -Uri  https://download.sysinternals.com/files/AutoLogon.zip -UseBasicParsing -OutFile 'C:\\auto.zip'
-Expand-Archive -Path 'C:\\auto.zip' -DestinationPath 'C:\\' -Force
+Expand-Archive -Path 'C:\\auto.zip' -DestinationPath 'C:\\Users\\zdx\\desktop\\' -Force
 Invoke-WebRequest -Uri  https://d32a6ru7mhaq0c.cloudfront.net/Zscaler-windows-4.3.0.131-installer-x64.msi -UseBasicParsing -OutFile 'C:\\zinstaller.msi'
 msiexec /i C:\zinstaller.msi /quiet CLOUDNAME=zscalerthree HIDEAPPONLAUNCHUI=1 USERDOMAIN=thezerotrustexchange.com
 
