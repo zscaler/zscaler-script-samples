f3f52acbd75984022f1b241af9c6ff19f693e93b
vikasksrivastava <vikas.k.srivastava@gmail.com>
2023-10-26 14:47:07 -0400

    Update setup1.ps1

--- ZDX/utils/setup1.ps1
+++ ZDX/utils/setup1.ps1
@@ -8,10 +8,11 @@ $file = "C:\Users\zdx\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Star
 
 Invoke-WebRequest -Uri  https://download.sysinternals.com/files/AutoLogon.zip -UseBasicParsing -OutFile 'C:\\auto.zip'
 Expand-Archive -Path 'C:\\auto.zip' -DestinationPath 'C:\\' -Force
-Start-Process $autologon -ArgumentList $username,$domain,$password
 Invoke-WebRequest -Uri  https://d32a6ru7mhaq0c.cloudfront.net/Zscaler-windows-4.3.0.131-installer-x64.msi -UseBasicParsing -OutFile 'C:\\zinstaller.msi'
 msiexec /i C:\zinstaller.msi /quiet CLOUDNAME=zscalerthree HIDEAPPONLAUNCHUI=1 USERDOMAIN=thezerotrustexchange.com
 
 # Creating startup file 
 New-Item $file -ItemType File -Value "timeout /t 86400 /nobreak`n"
 Add-Content $file "shutdown -r"
+
+Start-Process $autologon -ArgumentList $username,$domain,$password
